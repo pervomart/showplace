716a6ede8961f9e2167b95bf0846b2d8a0097bbb
Dmitry Grinenko <dmitry@grinenko.ru>
2020-06-15 17:27:59 +0300

    Updates

---
+++ precache-manifest.b041946044bf8646bcde88f6b31034ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e000fb03eeffbd418a41404d5203a563",
+    "url": "/showplace/index.html"
+  },
+  {
+    "revision": "d00a302f4f2f72b0615c",
+    "url": "/showplace/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "d05d5eff68c7ca54b352",
+    "url": "/showplace/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d00a302f4f2f72b0615c",
+    "url": "/showplace/static/js/2.78e56f32.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/showplace/static/js/2.78e56f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05d5eff68c7ca54b352",
+    "url": "/showplace/static/js/main.b7c54ae6.chunk.js"
+  },
+  {
+    "revision": "4a6f449cda62e6525716",
+    "url": "/showplace/static/js/runtime-main.2e6ebd93.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/showplace/static/media/persik.4e1ec840.png"
+  }
+]);
