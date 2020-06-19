f171fe51d9cab440f2be30ce2ca008d696a07e40
RioDel <daniokipaky@mail.ru>
2020-06-19 19:34:50 +0300

    dan

--- src/panels/Main.js
+++ src/panels/Main.js
@@ -27,13 +27,13 @@ const Main = ({ id, go, fetchedUser, fetchedGeo }) => (
 
         <Group title="Страна">
      class Example extends React.Component {
-  constructor(props) {
+constructor(props) {
     super(props);
 
     this.state = {
       activeView: 'view1'
     }
-  }
+}
 
   render() {
     return (
